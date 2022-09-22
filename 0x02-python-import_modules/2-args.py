8958a16c5439d4e8e778deb54614e77e8df09a9e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-22 04:51:48 -0700

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ n = len(sys.argv)
 if n == 1:
     print("{} {}".format(n - 1, "arguments."))
 else:
-    print("{} {}".format(n - 1, "arguments:"))
+    if n == 2:
+        print("{} {}".format(n - 1, "argument:"))
+    else:
+        print("{} {}".format(n - 1, "arguments:"))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
