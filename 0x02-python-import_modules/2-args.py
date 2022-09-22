30148eb0f569ded78426d35d22f55418834da923
Bezawit11 <socbeza13@gmail.com>
2022-09-22 04:43:45 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
 n = len(sys.argv)
-print("{} {}". format(n - 1, "arguments:"))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+if n == 1:
+    print("{} {}".format(n - 1, "arguments."))
+else:
+    print("{} {}".format(n - 1, "arguments:"))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
