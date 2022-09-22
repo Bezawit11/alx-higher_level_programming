89202df0aae6df153c15479f194a18acea74c4c0
Bezawit11 <socbeza13@gmail.com>
2022-09-22 06:07:38 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     list = dir(hidden_4)
     for i in range(len(list)):
-        if list[:2] != "__":
+        if list[i][:2] != "__":
             print(list[i])
