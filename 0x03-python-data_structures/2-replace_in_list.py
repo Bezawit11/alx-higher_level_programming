261bd5cd554c7c173010cd28462011860dd606b7
Bezawit11 <socbeza13@gmail.com>
2022-09-23 02:05:28 -0700

    task 4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for i in range (len(my_list)):
-        if i == idx:
+        if i == idx and idx > 0:
             my_list[i] = element
         print(my_list[i])
