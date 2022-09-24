a73204fafca72520b0f9e69ca26c7fdc3dd53b02
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-23 22:55:17 -0700

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-   str1 = str(my_string)
-   str1 = str1.translate({ord("c"): None})
-   str1 = str1.translate({ord("C"): None})
-   return str1
+    str1 = str(my_string)
+    str1 = str1.translate({ord("c"): None})
+    str1 = str1.translate({ord("C"): None})
+    return str1
