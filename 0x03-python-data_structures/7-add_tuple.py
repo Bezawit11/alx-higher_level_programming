c773c47d3b3c26e84958cf17d94c651eae729c98
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-25 06:50:06 -0700

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,7 +8,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         d = (0, 0)
     if len(tuple_a) == 1:
         c = (tuple_a[0], 0)
-    if len(b) == 1:
+    if len(tuple_b) == 1:
         d = (tuple_b[0], 0)
     tup = tuple((c[0] + d[0], c[1] + d[1]))
     return tup
