adb2ba42c65c8dd087c2f8d9fa74f87610834a2a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-25 07:00:59 -0700

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = tuple((len(sentence), sentence[0]))
-    return tup 
+    if sentence == "":
+       tup = tuple((0, "")) 
+    else:
+        tup = tuple((len(sentence), sentence[0]))
+    return tup
