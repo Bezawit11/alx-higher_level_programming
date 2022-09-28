ad9aa85374613d5e898aa8fe1d47b53606130c66
Bezawit11 <socbeza13@gmail.com>
2022-09-28 14:57:27 -0700

    task 14

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return None
+    r = []
+    for i in range(len(matrix[0])):
+        k = list(map(lambda x: x[i]*x[i], matrix))
+        r.append(k)
+    new = [[r[j][i] for j in range(len(r))] for i in range(len(r[0]))]
+    return new
