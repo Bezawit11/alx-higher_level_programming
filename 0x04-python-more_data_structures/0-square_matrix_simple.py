bbbaba9508ccf06b5fad2dc443db26658b231b46
Bezawit11 <socbeza13@gmail.com>
2022-09-28 08:24:31 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
     r = []
-    for i in range(len(matrix)):
+    for i in range(len(matrix[0])):
         k = list(map(lambda x: x[i]*x[i], matrix))
         r.append(k)
     new = [[r[j][i] for j in range(len(r))] for i in range(len(r[0]))]
