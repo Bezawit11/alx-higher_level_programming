b3fec7ba7551807797118b2812432cc0e5bda4dd
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-28 14:12:10 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new = list(matrix)
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-
+           new[i][j] = matrix[i][j] * matrix[i][j]
+    return new
