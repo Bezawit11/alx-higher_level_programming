2a2fe870c4dec0d8fadcf37802eca35aaf2e9800
Bezawit11 <socbeza13@gmail.com>
2022-09-28 12:39:36 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    sum = 0
+    for x in a_dictionary:
+        if x == key:
+            sum = 1
+            a_dictionary[x] = value
+    if sum == 0
+        a_dictionary[key] = value
+    return a_dictionary
