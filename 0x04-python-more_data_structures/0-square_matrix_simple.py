c205faabffc72dd56911954f4bfdd414b902b4e2
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-29 07:53:18 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return None
+    if matrix == [[]]:
+        return [[]]
     r = []
     for i in range(len(matrix[0])):
         k = list(map(lambda x: x[i]*x[i], matrix))
