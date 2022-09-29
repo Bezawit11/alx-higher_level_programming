a04ef468df448384c5924740796f73f764fce5c9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-29 08:32:45 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -6,4 +6,4 @@ def simple_delete(a_dictionary, key=""):
         if x == key:
             a_dictionary.pop(key)
             return a_dictionary
-        return a_dictionary
+    return a_dictionary
