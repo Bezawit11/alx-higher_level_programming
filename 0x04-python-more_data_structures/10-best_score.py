d63d1c91a7da15a8980f7e4a494e1bc79f95a29a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-09-29 09:25:57 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     a = 0
     if a_dictionary == {}:
         return
+    if a_dictionary is None:
+        return None
     for x in a_dictionary:
         if a_dictionary[x] > a:
             a = a_dictionary[x]
