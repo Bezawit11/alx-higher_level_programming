c9f5d6b904a1dd7d04bcfa55da53d1db591331ed
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-10 18:16:41 +0100

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             res = my_list_1[i] / my_list_2[i]
             list.append(res)
-        except:
+        except (TypeError, ZeroDivisionError, IndexError):
             list.append(0)
         finally:
             if i >= len(my_list_1) or i >= len(my_list_1):
