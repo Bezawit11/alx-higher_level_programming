f89d6de4241ea99b439e1a13ae62b37bbec6662e
Bezawit11 <socbeza13@gmail.com>
2022-10-11 08:40:44 -0700

    added

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Node:
+    def __init__(self, data, next_node=None):
+        self.__data = data
+        self.__next_node = next_node
+    @property
+    def data(self):
+        return self.__data
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+    @property
+    def next_node(self):
+        return self.__next_node
+    @next_node.setter
+        def next_node(self, value):
+            if not isinstance(value, None):
+                raise TypeError("data must be an integer")
+            self.__next_node = value
+class SinglyLinkedList:
+        def __init__(self):
+            self.__head = None
+        def sorted_insert(self, value):
+            if self.__head is None:
+                new.next_node = None
+                self.__head = new
