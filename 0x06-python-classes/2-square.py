7cdb6f4b6ec57f1b3986c61a49a257ef0e7864ff
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-11 10:32:57 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+""""Square module."""
+
+
 class Square:
+    """defines a square class"""
+    
     def __init__(self, size=0):
+        """Constructor
+        
+        Args: 
+            size: size of a square
+        Raises:
+            ValueError: if the size is negative
+         """
         if int(size) < 0:
-            raise Exception("size must be >= 0")
+            raise ValueError("size must be >= 0")
         try:
             self.__size = size
         except TypeError in e:
