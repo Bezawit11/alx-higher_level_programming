5f671bb8402fea261ea6a9d254b6a0d4fcddc7b1
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-11 10:27:44 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
+    """Defines a square class"""
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: size of a square
+        """
         self.__size = None
