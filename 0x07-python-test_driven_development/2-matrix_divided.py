5d592ecd1bc56d473dd627d1cc2477ac53e400e5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-15 10:23:26 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    list = []
+    mylist = []
     ins = []
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(a, list) for a in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for i in range(len(matrix)):
         if i != 0 and len(matrix[i]) != len(matrix[i - 1]):
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(matrix[i])):
-                ins.append(matrix[i][j] / div)
-        list.append(ins)
+            if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            ins.append(matrix[i][j] / div)
+        mylist.append(ins)
         ins = []
-    return list
+    return mylist
