944e3e70e0ec615a62319a4d6bad4665a2d939ae
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-15 10:29:27 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,7 +14,7 @@ def matrix_divided(matrix, div):
         for j in range(len(matrix[i])):
             if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            ins.append(matrix[i][j] / div)
+            ins.append(round(matrix[i][j] / div, 2))
         mylist.append(ins)
         ins = []
     return mylist
