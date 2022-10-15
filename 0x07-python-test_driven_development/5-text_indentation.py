bf4e9133ccb57d1b6f47699f4b5069050e1edea0
Bezawit11 <socbeza13@gmail.com>
2022-10-15 05:55:37 -0700

    print square

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for s in text:
+        if s == '.' or s == '?' or s == '!':
+            print(s)
+        else:
+            print(s, end="")
