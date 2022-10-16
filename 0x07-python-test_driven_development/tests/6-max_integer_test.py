7c33bdba7e7d57b9ac6f757f7eadc6a95ace300f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-16 13:43:59 +0100

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,6 +2,26 @@
 
 import unittest
 class TestMaxInteger(unittest.TestCase):
-    def test_all_negative(self):
+    def test_all_positive(self):
         l = [1, 2, 8, 3]
         self.assertEqual(max_integer(l), 8)
+    
+    def test_all_equal(self):
+        m = [3, 3, 3, 3]
+        self.assertEqual(max_integer(m), 3)
+        
+    def test_all_negative(self):
+        n = [-2, -5, -7, -4]
+        self.assertEqual(max_integer(n), -2)
+        
+    def test_string(self):
+        o = [1, "2", 8, 3]
+        with self.assertRaises(TypeError)
+            max_integer(string)
+            
+    def test_empty(self):
+        p = []
+        self.assertEqual(max_integer(p), None)
+        
+if __name__ == '__main__':
+    unittest.main()
