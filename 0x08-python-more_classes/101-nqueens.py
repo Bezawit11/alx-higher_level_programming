85f29d3087737edd90d0fecf3a16d7ebdbe0c921
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-18 13:10:07 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -24,18 +24,23 @@ def checker(i , j , z, h, l):
             return 0
     return 1
 
+def queen(matrix):
+    l = []
+    ins = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            for z in range(len(matrix)):
+                for h in range(len(matrix)):
+                    if z != i and h != j:
+                        if checker(i , j , z, h, len(matrix)) == 1:
+                            print(matrix[z][h], end=",") 
+            print(" for " + str(i) + " and " + str(j)) 
 
 matrix = [
-        [1, 2, 3],
-        [4, 5, 6,],
-        [7, 8 , 9],
-    ]
-for i in range(3):
-    for j in range(3):
-        for z in range(3):
-            for h in range(3):
-                if z != i and h != j:
-                    if checker(i , j , z, h, len(matrix)) == 1:
-                        print(matrix[z][h], end="")
-        print()            
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
 
+queen(matrix)
