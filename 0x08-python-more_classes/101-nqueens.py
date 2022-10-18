9cce8341ccf92e59f59483ed77ac3840f8ee893d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-18 12:45:09 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
-l = []
-ins = []
+def checker(i , j , z, h, l):
+    for k in range(1, l):
+        if z != i + k or h != j + k:
+            continue
+        else:
+            return 0
+    for k in range(1, l):
+        if z != i - k or h != j + k:
+            continue
+        else:
+            return 0;
+            
+    for k in range(1, l):
+        if z != i - k or h != j - k:
+            continue
+        else:
+            return 0
+            
+    for k in range(1, l):
+        if z != i + k or h != j - k:
+            continue
+        else:
+            return 0
+    return 1
+
+
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6,],
+        [7, 8 , 9],
+    ]
 for i in range(3):
     for j in range(3):
         for z in range(3):
             for h in range(3):
-                if z != i and h != j:  #can't be the same r & c
-                    if z != i + a or h != j + a or z != i - a or h != j - a or z != i - a or h != j + a or z != i + a or h != j - a:
-                        print(m[z][h], end="")
-                        a += 1
-        print()
+                if z != i and h != j:
+                    if checker(i , j , z, h, len(matrix)) == 1:
+                        print(matrix[z][h], end="")
+        print()            
+
