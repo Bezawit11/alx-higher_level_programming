14b475fb81833af4dcfe6caa6132fc3b0d4e7791
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-19 06:22:06 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,7 +12,7 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for s in text:
-        if s == '.' or s == '?' or s == '!':
-            print(s)
-        else:
-            print(s, end="")
+        print(s, end="")
+        if s == '.' or s == '?' or s == ':':
+            print()
+            print()
