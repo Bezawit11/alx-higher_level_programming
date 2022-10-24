0b151ec7b03236ea4b02e05952a492a4e60f7439
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-24 15:21:12 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""creating an empty class"""
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry
+    """
+    pass
+    def area(self):
+        """
+        finds 
+        """
+        raise Exception("area() is not implemented")
