ec2a727ffa6d110ae438a610403e11e4e9e7953b
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-24 15:14:26 +0100

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """creating an empty class"""
 
+
 class BaseGeometry:
-  """
-  an empty class BaseGeometry
-  """
-  pass
+    """
+    an empty class BaseGeometry
+    """
+    pass
