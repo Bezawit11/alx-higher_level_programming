1f1b4ed5e1064e7654781fa47b9a1f4c51452173
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 18:59:34 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -10,4 +10,4 @@ def from_json_string(my_str):
       a function that returns an object (Python data structure)
       represented by a JSON string:
     """
-    return json.dumps(my_str)
+    return json.loads(my_str)
