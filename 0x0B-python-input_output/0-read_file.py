565fb87ec06b2275e65d88feaded4871c7b0cefa
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 13:58:28 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ reads"""
+
+
 def read_file(filename=""):
-  """
+    """
      a function that reads a file
-  """
+    """
     f = open('filename', 'r', encoding="utf-8")
     print(f)
