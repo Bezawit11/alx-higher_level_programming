8f7c8d69f388a1029a4dbcf3a64428e71aa184c2
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 19:54:23 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,10 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-dic = load_from_json_file("add_item.json")
-dic.extend(sys.argv[1:])
-save_to_json_file(dic, "add_item.json")
+    dict = load_from_json_file("add_item.json")
+    dict.extend(sys.argv[1:])
+    save_to_json_file(dict, "add_item.json")
