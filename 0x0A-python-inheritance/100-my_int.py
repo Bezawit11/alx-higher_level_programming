a29e4406cc09add451c951c8163f6bec69e7e94d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 11:06:26 +0100

    Update 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+class MyInt(int):
+    def __init__(self, n):
+        self.n = n
+  
+    def __eq__(self, other):
+        return self.n != other
+
+    def __ne__(self, other):
+        return self.n == other
