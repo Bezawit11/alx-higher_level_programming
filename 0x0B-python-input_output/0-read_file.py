c70ec62146ac143e2dcfed05466bd11e9ef4474f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 14:05:06 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """
      a function that reads a file
     """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f)
