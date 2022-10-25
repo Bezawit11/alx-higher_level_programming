6970ce1ea54e3077e5b07e96f74c02b313128d15
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 09:11:22 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,8 +12,8 @@ class BaseGeometry:
         """
         Initialize
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def area(self):
         """
