a659842581193bee4ad301d6a37ab282d4641667
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-25 11:10:17 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,20 @@
+#!/usr/bin/python3
+"""the rebellious class lol"""
+
+
 class MyInt(int):
+    """
+       a class MyInt that inherits from int
+    """
     def __init__(self, n):
         self.n = n
   
     def __eq__(self, other):
+        """== operators inverted
+        """
         return self.n != other
 
     def __ne__(self, other):
+        """!= operators inverted
+        """
         return self.n == other
