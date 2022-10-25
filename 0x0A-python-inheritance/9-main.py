2bd1e4e167ba8aa53fb755bc9d2eed42f402d853
Bezawit11 <socbeza13@gmail.com>
2022-10-25 01:05:32 -0700

    task 9

--- 0x0A-python-inheritance/9-main.py
+++ 0x0A-python-inheritance/9-main.py
@@ -2,12 +2,25 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
-
-r = Rectangle(3, 5)
-
-
-
-print(r)
-
-print(r.area())
+try:
+    r = Rectangle()
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+try:
+    r = Rectangle(1)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+try:
+    r = Rectangle(1, [12, 52])
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+try:
+    r = Rectangle(4, 5)
+    print(r.width)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+try:
+    r = Rectangle(4, 5)
+    print(r.height)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
