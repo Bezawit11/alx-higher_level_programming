8c83980037a816c3cc81d60179154fd74e2747d6
Bezawit11 <socbeza13@gmail.com>
2022-10-25 11:36:35 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""ghj"""
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+dic = load_from_json_file("add_item.json")
+dic.extend(sys.argv[1:])
+save_to_json_file(dic, "add_item.json")
