87a67fc66c89eaa3d5a08f24e3a5e71252e95c42
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-26 10:33:23 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,15 @@
 """ghj"""
 
 import sys
+import json
 import os.path
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+dict = []
 
 if os.path.exists("add_item.json"):
         dict = load_from_json_file("add_item.json")
-        dict.append(sys.argv[1:])
-        save_to_json_file(dict, "add_item.json")
+dict.append(sys.argv[1:])
+save_to_json_file(dict, "add_item.json")
