7bf97e0ce79b7ae7ae135e206561c49614255226
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-26 09:56:40 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,5 +10,5 @@ def load_from_json_file(filename):
        a function that creates an
        Object from a “JSON file”:
     """
-    with open(filename) as f:
+    with open(filename, 'w') as f:
         return json.load(f)
