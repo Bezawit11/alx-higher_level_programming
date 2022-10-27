2009094ad8f9c074a34c71af6f56330686234eb1
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-27 16:56:16 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Child class Rectangle"""
+from models.base import Base
+
+
 class Rectangle(Base):
+    """Child class of Base
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
