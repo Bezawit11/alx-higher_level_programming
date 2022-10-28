c5be2e4ec209442ba098db7200e9ff36a0cc14c4
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-28 15:31:32 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Base:
+    """Base class"""
     __nb_objects = 0
     def __init__(self, id=None):
         if id != None:
@@ -7,3 +8,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
