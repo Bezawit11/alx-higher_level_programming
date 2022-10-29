dc18007af6b24f93e80ab49599771bb0284d636a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-29 19:18:13 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,6 +26,21 @@ class Square(Rectangle):
         """returns the dictionary representation of a Square"""
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
     
+    def update(self, *args, **kwargs):
+        """ assigns attributes
+        """
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                self.__setattr__(key, value)
+            return
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
+        
     def __str__(self):
         """returns the string representation of the class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
