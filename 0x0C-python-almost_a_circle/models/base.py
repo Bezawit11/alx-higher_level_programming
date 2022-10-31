02375aa2475b91f0ebe87f315a5ccab986dc72c6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-31 16:31:54 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,12 +8,12 @@ class Base:
     """Base class"""
     __nb_objects = 0
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries
@@ -21,7 +21,7 @@ class Base:
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
-    
+
     @staticmethod
     def from_json_string(json_string):
         """eturns the list of the JSON string representation
@@ -29,14 +29,14 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
-    
+
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set
         """
         from models.square import Square
         from models.rectangle import Rectangle
-        
+
         if cls.__name__ == "Rectangle":
             r = Rectangle(2, 4)
         elif cls.__name__ == "Square":
@@ -47,15 +47,15 @@ class Base:
     def save_to_file(cls, list_objs):
         """ writes the JSON string representation of list_objs to a file
         """
-        if list_objs != None:
-            l = []
+        if list_objs is not None:
+            k = []
             file = cls.__name__ + ".json"
             for a in list_objs:
                 rep = cls.to_json_string(a.to_dictionary())
-                l.append(json.loads(rep))
+                k.append(json.loads(rep))
         with open(file, "w") as f:
-            json.dump(l, f)
-    
+            json.dump(k, f)
+
     @classmethod
     def load_from_file(cls):
         """returns an instance with all attributes already set
@@ -66,8 +66,8 @@ class Base:
                 m = cls.from_json_string(f.read())
         except:
             return []
-        l = []
+        k = []
         for a in m:
             s = cls.create(**a)
-            l.append(s)
-        return l
+            k.append(s)
+        return k
