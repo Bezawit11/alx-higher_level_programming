8686cd5b56fb7c2fdbd8823f315d0686ca7c36e6
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-10-31 17:08:46 +0000

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,12 +8,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
-    
+
     @property
     def size(self):
         """returns size value"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """size value setter"""
@@ -21,11 +21,11 @@ class Square(Rectangle):
         self.__size = value
         self.width = value
         self.height = value
-        
+
     def to_dictionary(self):
         """returns the dictionary representation of a Square"""
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
-    
+
     def update(self, *args, **kwargs):
         """ assigns attributes
         """
@@ -40,7 +40,7 @@ class Square(Rectangle):
             self.y = args[3]
         except IndexError:
             pass
-        
+
     def __str__(self):
         """returns the string representation of the class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
