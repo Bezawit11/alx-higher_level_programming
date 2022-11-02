7b9794d5590208fd7cf81f67929678a51410d37c
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-02 08:41:51 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""Roman Numbers"""
 
+def roman_to_int(roman_string):
+    """a function that converts a Roman numeral to an integer"""
+    sum = 0
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    if roman_string == "IV":
+        return 4
+    elif roman_string == "IX":
+        return 9
+    for i in range (len(roman_string)):
+        if roman_string[i] == "I":
+           sum += 1
+        elif roman_string[i] == "V":
+            sum += 5
+        elif roman_string[i] == "X":
+            sum += 10
+        elif roman_string[i] == "L":
+            sum += 50
+        elif roman_string[i] == "C":
+            sum += 100
+        elif roman_string[i] == "D":
+            sum += 500
+    return sum
