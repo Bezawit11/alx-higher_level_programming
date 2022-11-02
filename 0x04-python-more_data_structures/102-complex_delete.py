31d67a42f9ea2e73ad92c158dceed01613081c53
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-02 08:20:35 +0000

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""complex delete"""
+
+
 def complex_delete(a_dictionary, value):
+    """a function that deletes keys with a specific value in a dictionary"""
     t = a_dictionary.copy()
-    for a in a_dictionary:
-        if a_dictionary[a] == value:
-            t.pop(a)
-    return t
+    for a in t:
+        if t[a] == value:
+            a_dictionary.pop(a)
+    return a_dictionary
