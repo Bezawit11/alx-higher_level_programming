5b0856eaccbade60d20f07e1ac6321ebbd2115ab
Bezawit11 <socbeza13@gmail.com>
2022-11-19 00:06:45 -0800

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,7 @@
 """ghj"""
 
 
-import json
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
     """
-    jsonStr = json.dumps(obj.__dict__)
-    return jsonStr
+    return obj.__dict__
