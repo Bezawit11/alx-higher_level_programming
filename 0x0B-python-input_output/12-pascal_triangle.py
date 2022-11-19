7473ec4f8179eea0919969c04e771294f09a0b99
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-19 07:53:10 +0000

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,25 +6,26 @@ def factorial(n):
     """Calculate factorial
     """
     f = 1
-    for i in range(1,n+1):
+    for i in range(1, n + 1):
         f = f * i
     return f
 
+
 def pascal_triangle(n):
-    """returns a list of lists of integers 
+    """returns a list of lists of integers
        representing the Pascal’s triangle of n
     """
-    l = []
+    a = []
     s = []
     if n <= 0:
-        return l
+        return a
     else:
         for i in range(n):
             for j in range(i + 1):
                 x = factorial(i)
                 y = factorial(i - j)
                 z = factorial(j)
-                l.append(int(x / (y * z)))
-            s.append(l)
-            l = []
+                a.append(int(x / (y * z)))
+            s.append(a)
+            a = []
         return s
