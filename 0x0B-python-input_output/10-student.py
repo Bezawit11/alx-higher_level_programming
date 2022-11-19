e3357adff4361c18d9bb5ad633257c2c61baeb90
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-19 08:55:34 +0000

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""a class named student"""
+
+
+class Student:
+    """defines a students name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        if attrs is not None:
+            d = {}
+            for s in attrs:
+                if s == "first_name":
+                    d[s] = self.first_name
+                elif s == "last_name":
+                    d[s] = self.last_name
+                elif s == "age":
+                    d[s] = self.age
+            return d
+        return self.__dict__
