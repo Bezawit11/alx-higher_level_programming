17f5e3314964c861248c47b8e8e238a7b6945918
Bezawit11 <socbeza13@gmail.com>
2022-12-19 20:28:28 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,16 +2,16 @@
 function second_biggest(arr){
 const b = []
 for (let i = 2; i < arr.length; i++){
-b[i - 2] = arr[i];
+b[i - 2] = parseInt(arr[i]);
 }
 b.sort()
 b.reverse()
 return b[1]
 }
 
-if (isNaN(parseInt(process.argv[2]))){
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))){
   console.log("0")
 }
 else{
-console.log(second_biggest(parseInt(process.argv)))
+console.log(second_biggest(process.argv))
 }
