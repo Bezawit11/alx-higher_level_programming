49c2d81574072afe68620aa6637c215c1db2c820
Bezawit11 <socbeza13@gmail.com>
2022-12-19 21:41:19 +0000

    semi standard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-function factorial(a){
-    if (a == 1){
-        return 1;
-    }
-    return a * factorial(a -1)
+function factorial (a) {
+  if (a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 }
-if (isNaN(parseInt(process.argv[2]))){
-  console.log("1")
-}
-else{
- console.log(factorial(parseInt(process.argv[2])))
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('1');
+} else {
+  console.log(factorial(parseInt(process.argv[2])));
 }
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-function second_biggest(arr){
-const b = []
-for (let i = 2; i < arr.length; i++){
-b[i - 2] = parseInt(arr[i]);
-}
-b.sort()
-b.reverse()
-return b[1]
+function SecondBiggest (arr) {
+  const b = [];
+  for (let i = 2; i < arr.length; i++) {
+    b[i - 2] = parseInt(arr[i]);
+  }
+  b.sort();
+  b.reverse();
+  return b[1];
 }
 
-if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))){
-  console.log(0)
-}
-else{
-console.log(second_biggest(process.argv))
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+  console.log(0);
+} else {
+  console.log(SecondBiggest(process.argv));
 }
