e48db19a9ce1ac7d4710e6cc1a5587b9378c3392
Bezawit11 <socbeza13@gmail.com>
2022-12-19 21:14:25 +0000

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-myVar = process.argv
-if (myVar.length === 2) 
-{
-  console.log("No argument");
-} 
-else if (myVar.length === 3) {
-  console.log("Argument found");
-} 
-else {
-  console.log("Arguments found");
+const myVar = process.argv;
+if (myVar.length === 2) {
+  console.log('No argument');
+} else if (myVar.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
