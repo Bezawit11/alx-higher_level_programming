038b9fcd70b0f5d32c3ae0065a18db96a18b390d
Bezawit11 <socbeza13@gmail.com>
2022-12-20 09:17:41 +0000

    task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
-myObject["incr"] = function() {
-    myObject["value"] = myObject["value"] + 1;
+myObject.incr = function () {
+  myObject.value = myObject.value + 1;
 };
 myObject.incr();
 console.log(myObject);
