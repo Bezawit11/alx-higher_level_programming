ce8d28e65c28172e8397b779450e14f1ebf1d90f
Bezawit11 <socbeza13@gmail.com>
2022-12-20 14:20:08 +0000

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-let c = 0;
-for (let i = 0; i < list.length; i++) {
-  if (searchElement === list[i]){
-    c += 1;
+  let c = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      c += 1;
+    }
   }
-}
-return c;
+  return c;
 };
