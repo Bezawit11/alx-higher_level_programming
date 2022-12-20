f26f245463c4bc799ace38a1740b289f445209f8
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-20 14:07:45 +0000

    Update 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+module.exports = class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++){
+          for (let j = 0; j < this.width; j++){
+              process.stdout.write("X");
+          }
+          console.log("");
+      }
+  }
+};
