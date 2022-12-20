bb8ea3095e92ec0dd794656bd7ae80b2dec6f342
Bezawit11 <socbeza13@gmail.com>
2022-12-20 14:09:10 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,11 +5,11 @@ module.exports = class Square extends SquareParent {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++){
-          for (let j = 0; j < this.width; j++){
-              process.stdout.write("X");
-          }
-          console.log("");
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
       }
+      console.log('');
+    }
   }
 };
