c42f2df4dc67db54837af0999ed2df67c5a59de5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-30 09:00:44 +0000

    Update 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
 --  script that lists all the tables of a database in your MySQL server.
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type='BASE TABLE'
-      AND table_schema = 'mysql'
+SHOW tables;
