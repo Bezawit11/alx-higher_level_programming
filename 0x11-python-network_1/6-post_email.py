5aa4db790a0b3c108b980eac88447c1d52e7af40
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 20:04:34 +0000

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address, sends a POST request to the 
-passed URL with the email as a parameter, and finally displays the body of the response
+Python script that takes in a URL and an email address,
+sends a POST request to the 
+passed URL with the email as a parameter,
+and finally displays the body of the response
 """
 
 if __name__ == '__main__':
@@ -11,4 +13,3 @@ if __name__ == '__main__':
     values['email'] = sys.argv[2]
     r = requests.post(sys.argv[1], data=values)
     print(r.text)
-    
