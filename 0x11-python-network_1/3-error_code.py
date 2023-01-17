96e08fd4b6c6531dfd1b959889c1489d37e9d1cd
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 20:01:28 +0000

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to 
+"""Python script that takes in a URL, sends a request to
 the URL and displays the body of the response (decoded in utf-8).
 """
 
@@ -12,4 +12,3 @@ if __name__ == '__main__':
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
-    
