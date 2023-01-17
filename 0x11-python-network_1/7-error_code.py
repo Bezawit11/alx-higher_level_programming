b5febf5240ef1fa369d2924b5334a5e693bfcef4
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 19:23:28 +0000

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ URL and displays the body of the response.
 if __name__ == '__main__':
     import requests
     import sys
-    try:
-        r = requests.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
         print(r.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error code: {e.status_code}")
