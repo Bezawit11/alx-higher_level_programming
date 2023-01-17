f663fad1d864bbebece46556e781039190a8f3f5
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 20:06:51 +0000

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the 
+Python script that takes in a URL, sends a request to the
 URL and displays the body of the response.
 """
 
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     import requests
     import sys
     r = requests.get(sys.argv[1])
-    a =  r.status_code
+    a = r.status_code
     if a >= 400:
         print(f"Error code: {a}")
     else:
