df86de10265f35306ae34e620c5801fff307a97c
Bezawit11 <socbeza13@gmail.com>
2023-01-17 11:59:25 -0800

    fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST 
+"""Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter
 """
 
@@ -14,4 +14,3 @@ if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
- 
