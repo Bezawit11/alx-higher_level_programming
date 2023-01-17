fb75cbb225e1a6f999f9fe41abd6985f513b858f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 18:59:38 +0000

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
     import requests
     import sys
     r = requests.get(sys.argv[1])
-    print(r.headers["X-Request-Id"])
-     
+    print(r.headers["X-Request-Id"])   
