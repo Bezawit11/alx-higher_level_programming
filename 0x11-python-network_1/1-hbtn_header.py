352cf955f281bed9ef37cc2f958cf097f8310ad8
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-01-17 17:58:10 +0000

    Update 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request
+ to the URL and displays the value of the X-Request-Id
+"""
+
+if __name__ == '__main__':
+    import sys
+    from urllib.request import urlopen
+    with urlopen(sys.argv) as response:
+        pass
+    response.headers["X-Request-Id"]
+ 
