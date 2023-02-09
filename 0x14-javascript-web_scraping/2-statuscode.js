721c55d611952fcfe6582450a1b597ade2fad00a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-09 08:24:41 +0000

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const r = require('request');
 r(process.argv[2], function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('code:', response && response.statusCode);
 });
