1a580121e50db226b3969b5940547722035de454
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-09 08:44:38 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@ const r = require('request');
 r('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   console.log('code:', response && response.statusCode);
   const info = JSON.parse(body);
-  console.log(info[['title'])
+  console.log(info['title'])
 });
