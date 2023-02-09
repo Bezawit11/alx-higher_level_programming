f4406de717bea04843fd7bdfc459bc89cd84430c
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-09 09:11:26 +0000

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,10 @@ const r = require('request');
 r(process.argv[2], function (error, response, body) {
   const info = JSON.parse(body);
   let count = 0
-  for a in info['results']:
-      for i in a['characters']:
-          if i == "https://swapi-api.alx-tools.com/api/people/18/":
-              count += 1
+  for (let a in info['results']){
+      for (i in a){
+        for (let j in i['characters']){
+              if j == "https://swapi-api.alx-tools.com/api/people/18/":
+                  count += 1}}}
   console.log(count)
 });
