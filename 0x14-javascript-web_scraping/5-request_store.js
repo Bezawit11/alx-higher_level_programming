e1a9144057fc926b668a3b5993a349b6d8302093
Bezawit11 <socbeza13@gmail.com>
2023-02-10 07:58:31 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const r = require('request');
 r(process.argv[2], function (error, response, body) {
 b = response['body'];
-console.log(b);
 fs.writeFile(process.argv[3], b,'utf-8', (err) => {
-
 });
 });
