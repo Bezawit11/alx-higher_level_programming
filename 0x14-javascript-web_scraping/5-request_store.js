3f5ae628bf88ccfdded45f94613f9d1da56f721c
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-10 07:57:11 +0000

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@ const r = require('request');
 r(process.argv[2], function (error, response, body) {
 b = response['body'];
 console.log(b);
-fs.writeFile(str(b), process.argv[3], 'utf-8', (err) => {
-if (err) throw err;
+fs.writeFile(process.argv[3], b,'utf-8', (err) => {
+
 });
 });
