6372410d0cc209cf5b9cbdb3233b26f051d81ebe
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-13 11:04:40 +0000

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,26 +4,26 @@ r(process.argv[2], function (error, response, body) {
   const info = JSON.parse(body);
   let count = 0
   let d = {}
-  if (info.length === 0){
+  if (info.length == 0){
     console.log(d)
 }
   else{
   let b = info[0]['userId']
     for (let i = 0; i < info.length; i++){
         a = info[i]['userId']
-    if (info[i]['userId'] === b){
-        if (info[i]['completed'] === true){
+    if (info[i]['userId'] == b){
+        if (info[i]['completed'] == true){
         count += 1}
     }
     else {
-        if (count !== 0){
+        if (count != 0){
         if (d[b] > 0){
         d[b] = d[b] + count}
         else{
             d[b] = count
         }
         }
-        if (info[i]['completed'] === true){
+        if (info[i]['completed'] == true){
             count = 1
         }
         else{
@@ -32,7 +32,7 @@ r(process.argv[2], function (error, response, body) {
         b = info[i]['userId']
     }
     }
-    if (count !== 0){
+    if (count != 0){
     if (d[b] > 0){
         d[b] = d[b] + count}
         else{
