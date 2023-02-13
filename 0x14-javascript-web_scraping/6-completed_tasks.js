0672fe382ff7e84f9ca70e4ee3f24ced30c735ff
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-13 11:42:59 +0000

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,6 +3,7 @@ const r = require('request');
 r(process.argv[2], function (error, response, body) {
   const info = JSON.parse(body);
   let count = 0;
+  let a = 0;
   const d = {};
   if (error) {
     console.error(error);
