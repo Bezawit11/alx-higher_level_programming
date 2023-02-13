8bb9b6eb4e2d7bce836b1956b77d654ee765676d
Bezawit11 <socbeza13@gmail.com>
2023-02-13 11:30:20 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const r = require('request');
-r(process.argv[2], function (response, body) {
+r(process.argv[2], function (error, response, body) {
   console.log('code:', response && response.statusCode);
 });
